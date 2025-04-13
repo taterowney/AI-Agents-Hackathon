91b09ac61cae888ac0b27e14524bf6069e1de65b
taterowney <51540870+taterowney@users.noreply.github.com>
2025-04-12 21:08:31 -0400

    Different hosting

--- eval.sh
+++ eval.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=1-00:00:00
-#SBATCH --gres=gpu:A6000:2
+#SBATCH --gres=gpu:A6000:1
 #SBATCH --mem=100G
 
 
@@ -15,7 +15,7 @@
 # --download_dir=models
 
 #vllm serve deepseek-ai/DeepSeek-R1-Distill-Qwen-14B --port 8000 --dtype=float32 --max_model_len 100000 --tensor-parallel-size 4 --enable-reasoning --reasoning-parser deepseek_r1 &
-vllm serve deepseek-ai/DeepSeek-R1-Distill-Qwen-14B --port 8000 --max_model_len 100000 --tensor-parallel-size 2 --enable-reasoning --reasoning-parser deepseek_r1 &
+vllm serve deepseek-ai/DeepSeek-R1-Distill-Qwen-14B --port 8000 --max_model_len 50000 --tensor-parallel-size 2 --enable-reasoning --reasoning-parser deepseek_r1 &
 
 echo "Waiting for vLLM server to start..."
 until curl -s http://localhost:8000/ping > /dev/null; do
